4d2f19aa9139ff888cd4e88d977291499965fe6b
Tzu-Mao Li <tzumao@mit.edu>
2023-12-24 14:13:53 -0800

    add comments to DuplicateVariable exception

--- error.py
+++ error.py
@@ -5,6 +5,9 @@ class UserError(Exception):
 
 @_attrs.define(frozen=True)
 class DuplicateVariable(UserError):
+    # the name of the variable that is duplicated
     var : str
+    # the line number where the variable is first declared
     first_lineno : int
+    # the line number where the variable is duplicatedly declared
     duplicate_lineno : int
