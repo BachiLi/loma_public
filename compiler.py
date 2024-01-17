7e129f26ede63836c5c441216f4c2f2ecd5c7a4b
Tzu-Mao Li <tzumao@mit.edu>
2024-01-16 17:01:27 -0800

    add printing for OpenCL code

--- compiler.py
+++ compiler.py
@@ -109,6 +109,9 @@ def compile(loma_code,
             print(log.stderr)
     elif target == 'opencl':
         code = codegen_opencl.codegen_opencl(structs, funcs)
+
+        print(code)
+        
         kernel_names = [func_name for func_name, func in funcs.items() if func.is_simd]
         lib = cl_utils.cl_compile(opencl_context,
                                   opencl_device,
