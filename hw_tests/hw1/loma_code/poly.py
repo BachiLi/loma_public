208e078631a1a846c8a9beb705385b25bbdd2501
Tzu-Mao Li <tzumao@mit.edu>
2024-02-06 15:07:37 -0800

    Fix HW1 poly test bug

--- hw_tests/hw1/loma_code/poly.py
+++ hw_tests/hw1/loma_code/poly.py
@@ -1,7 +1,7 @@
-def poly(x : In[float]):
+def poly(x : In[float]) -> float:
 	return 3 * x * x * x * x + 5 * x * x + 10
 
 d_poly = fwd_diff(poly)
 
-def d_poly_dx(x : In[float]):
+def d_poly_dx(x : In[float]) -> float:
 	return d_poly(x, 1.0).dval
