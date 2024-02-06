59ef9907b7c6ad630f1ad32c17adb5f9050d2f35
Tzu-Mao Li <tzumao@mit.edu>
2024-02-05 21:45:24 -0800

    Really fix HW1 test_struct_input this time

--- hw_tests/hw1/test.py
+++ hw_tests/hw1/test.py
@@ -213,8 +213,8 @@ def test_struct_input():
     df = Foo(4.56, -1)
     foo = _dFoo(f, df)
     out = lib.d_struct_input(foo)
-    assert abs(out.val - (5 * f.val.x + f.val.y - 1)) < epsilon and \
-           abs(out.dval - 5 * f.dval.x) < epsilon
+    assert abs(out.val - (5 * foo.val.x + foo.val.y - 1)) < epsilon and \
+           abs(out.dval - 5 * foo.dval.x) < epsilon
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
