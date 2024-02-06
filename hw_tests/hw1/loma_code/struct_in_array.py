52b3ba0621eb4ab1b7d5bfbd6119d8ea62b8ed09
Tzu-Mao Li <tzumao@mit.edu>
2024-02-06 14:48:41 -0800

    fix HW1 test bug

--- hw_tests/hw1/loma_code/struct_in_array.py
+++ hw_tests/hw1/loma_code/struct_in_array.py
@@ -3,7 +3,7 @@ class Foo:
     y : float
 
 def struct_in_array(in_f : In[Array[Foo]], out_f : Out[Array[Foo]]):
-    out_f[0].x = in_f.x * in_f.y
-    out_f[0].y = in_f.x + in_f.y
+    out_f[0].x = in_f[0].x * in_f[0].y
+    out_f[0].y = in_f[0].x + in_f[0].y
 
 d_struct_in_array = fwd_diff(struct_in_array)
