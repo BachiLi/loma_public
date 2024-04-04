ae9efa1bbc54dd71f88fd9c45b112a296ba14d8f
Tzu-Mao Li <tzumao@mit.edu>
2024-04-04 17:25:03 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -320,18 +320,18 @@ def test_call_stmt():
     assert lib.call_stmt() == 5
 
 def test_parallel_add():
-    with open('loma_code/parallel_add.py') as f:
-        structs, lib = compiler.compile(f.read(),
-                                        target = 'c',
-                                        output_filename = '_code/parallel_add.so')
-    py_x = [2, 3, 5]
-    x = (ctypes.c_int * len(py_x))(*py_x)
-    py_y = [7, 11, 13]
-    y = (ctypes.c_int * len(py_y))(*py_y)
-    py_z = [0, 0, 0]
-    z = (ctypes.c_int * len(py_z))(*py_z)
-    lib.parallel_add(x, y, z, len(py_z))
-    assert z[0] == 9 and z[1] == 14 and z[2] == 18
+    # with open('loma_code/parallel_add.py') as f:
+    #     structs, lib = compiler.compile(f.read(),
+    #                                     target = 'c',
+    #                                     output_filename = '_code/parallel_add.so')
+    # py_x = [2, 3, 5]
+    # x = (ctypes.c_int * len(py_x))(*py_x)
+    # py_y = [7, 11, 13]
+    # y = (ctypes.c_int * len(py_y))(*py_y)
+    # py_z = [0, 0, 0]
+    # z = (ctypes.c_int * len(py_z))(*py_z)
+    # lib.parallel_add(x, y, z, len(py_z))
+    # assert z[0] == 9 and z[1] == 14 and z[2] == 18
 
     with open('loma_code/parallel_add.py') as f:
         structs, lib = compiler.compile(f.read(),
@@ -346,55 +346,55 @@ def test_parallel_add():
     lib.parallel_add(x, y, z, len(py_z))
     assert z[0] == 9 and z[1] == 14 and z[2] == 18
 
-    cl_ctx, cl_device, cl_cmd_queue = cl_utils.create_context()
+    # cl_ctx, cl_device, cl_cmd_queue = cl_utils.create_context()
 
-    with open('loma_code/parallel_add.py') as f:
-        structs, lib = compiler.compile(f.read(),
-                                        target = 'opencl',
-                                        opencl_context = cl_ctx,
-                                        opencl_device = cl_device,
-                                        opencl_command_queue = cl_cmd_queue)
-    py_x = [2, 3, 5]
-    x = (ctypes.c_int * len(py_x))(*py_x)
-    py_y = [7, 11, 13]
-    y = (ctypes.c_int * len(py_y))(*py_y)
-    py_z = [0, 0, 0]
-    z = (ctypes.c_int * len(py_z))(*py_z)
+    # with open('loma_code/parallel_add.py') as f:
+    #     structs, lib = compiler.compile(f.read(),
+    #                                     target = 'opencl',
+    #                                     opencl_context = cl_ctx,
+    #                                     opencl_device = cl_device,
+    #                                     opencl_command_queue = cl_cmd_queue)
+    # py_x = [2, 3, 5]
+    # x = (ctypes.c_int * len(py_x))(*py_x)
+    # py_y = [7, 11, 13]
+    # y = (ctypes.c_int * len(py_y))(*py_y)
+    # py_z = [0, 0, 0]
+    # z = (ctypes.c_int * len(py_z))(*py_z)
 
-    status = ctypes.c_int32()
-    bufx = cl.clCreateBuffer(cl_ctx,
-                             cl.CL_MEM_READ_ONLY | cl.CL_MEM_COPY_HOST_PTR,
-                             ctypes.sizeof(x),
-                             ctypes.byref(x),
-                             ctypes.byref(status))
-    cl_utils.cl_check(status.value)
-    bufy = cl.clCreateBuffer(cl_ctx,
-                             cl.CL_MEM_READ_ONLY | cl.CL_MEM_COPY_HOST_PTR,
-                             ctypes.sizeof(y),
-                             ctypes.byref(y),
-                             ctypes.byref(status))
-    cl_utils.cl_check(status.value)
-    bufz = cl.clCreateBuffer(cl_ctx,
-                             cl.CL_MEM_WRITE_ONLY,
-                             ctypes.sizeof(z),
-                             None,
-                             ctypes.byref(status))
-    cl_utils.cl_check(status.value)
+    # status = ctypes.c_int32()
+    # bufx = cl.clCreateBuffer(cl_ctx,
+    #                          cl.CL_MEM_READ_ONLY | cl.CL_MEM_COPY_HOST_PTR,
+    #                          ctypes.sizeof(x),
+    #                          ctypes.byref(x),
+    #                          ctypes.byref(status))
+    # cl_utils.cl_check(status.value)
+    # bufy = cl.clCreateBuffer(cl_ctx,
+    #                          cl.CL_MEM_READ_ONLY | cl.CL_MEM_COPY_HOST_PTR,
+    #                          ctypes.sizeof(y),
+    #                          ctypes.byref(y),
+    #                          ctypes.byref(status))
+    # cl_utils.cl_check(status.value)
+    # bufz = cl.clCreateBuffer(cl_ctx,
+    #                          cl.CL_MEM_WRITE_ONLY,
+    #                          ctypes.sizeof(z),
+    #                          None,
+    #                          ctypes.byref(status))
+    # cl_utils.cl_check(status.value)
 
-    lib.parallel_add(bufx, bufy, bufz, len(py_z))
-    cl.clFinish(cl_cmd_queue)
+    # lib.parallel_add(bufx, bufy, bufz, len(py_z))
+    # cl.clFinish(cl_cmd_queue)
 
-    cl.clEnqueueReadBuffer(cl_cmd_queue,
-                           bufz,
-                           cl.CL_TRUE,
-                           0,
-                           ctypes.sizeof(z),
-                           ctypes.byref(z),
-                           0,
-                           None,
-                           None)
+    # cl.clEnqueueReadBuffer(cl_cmd_queue,
+    #                        bufz,
+    #                        cl.CL_TRUE,
+    #                        0,
+    #                        ctypes.sizeof(z),
+    #                        ctypes.byref(z),
+    #                        0,
+    #                        None,
+    #                        None)
 
-    assert z[0] == 9 and z[1] == 14 and z[2] == 18
+    # assert z[0] == 9 and z[1] == 14 and z[2] == 18
 
 def test_simd_local_func():
     with open('loma_code/simd_local_func.py') as f:
@@ -472,83 +472,6 @@ def test_simd_local_func():
                            None)
 
     assert z[0] == 9 and z[1] == 14 and z[2] == 18
-
-def test_parallel_add():
-    with open('loma_code/parallel_add.py') as f:
-        _, lib = compiler.compile(f.read(),
-                                  target = 'c',
-                                  output_filename = '_code/parallel_add.dll')
-    py_x = [2, 3, 5]
-    x = (ctypes.c_int * len(py_x))(*py_x)
-    py_y = [7, 11, 13]
-    y = (ctypes.c_int * len(py_y))(*py_y)
-    py_z = [0, 0, 0]
-    z = (ctypes.c_int * len(py_z))(*py_z)
-    lib.parallel_add(x, y, z, len(py_z))
-    assert z[0] == 9 and z[1] == 14 and z[2] == 18
-
-    # with open('loma_code/parallel_add.py') as f:
-    #     _, lib = compiler.compile(f.read(),
-    #                               target = 'ispc',
-    #                               output_filename = '_code/parallel_add_ispc.so')
-    # py_x = [2, 3, 5]
-    # x = (ctypes.c_int * len(py_x))(*py_x)
-    # py_y = [7, 11, 13]
-    # y = (ctypes.c_int * len(py_y))(*py_y)
-    # py_z = [0, 0, 0]
-    # z = (ctypes.c_int * len(py_z))(*py_z)
-    # lib.parallel_add(x, y, z, len(py_z))
-    # assert z[0] == 9 and z[1] == 14 and z[2] == 18
-
-    # cl_ctx, cl_device, cl_cmd_queue = cl_utils.create_context()
-
-    # with open('loma_code/parallel_add.py') as f:
-    #     _, lib = compiler.compile(f.read(),
-    #                               target = 'opencl',
-    #                               opencl_context = cl_ctx,
-    #                               opencl_device = cl_device,
-    #                               opencl_command_queue = cl_cmd_queue)
-    # py_x = [2, 3, 5]
-    # x = (ctypes.c_int * len(py_x))(*py_x)
-    # py_y = [7, 11, 13]
-    # y = (ctypes.c_int * len(py_y))(*py_y)
-    # py_z = [0, 0, 0]
-    # z = (ctypes.c_int * len(py_z))(*py_z)
-
-    # status = ctypes.c_int32()
-    # bufx = cl.clCreateBuffer(cl_ctx,
-    #                          cl.CL_MEM_READ_ONLY | cl.CL_MEM_COPY_HOST_PTR,
-    #                          ctypes.sizeof(x),
-    #                          ctypes.byref(x),
-    #                          ctypes.byref(status))
-    # cl_utils.cl_check(status.value)
-    # bufy = cl.clCreateBuffer(cl_ctx,
-    #                          cl.CL_MEM_READ_ONLY | cl.CL_MEM_COPY_HOST_PTR,
-    #                          ctypes.sizeof(y),
-    #                          ctypes.byref(y),
-    #                          ctypes.byref(status))
-    # cl_utils.cl_check(status.value)
-    # bufz = cl.clCreateBuffer(cl_ctx,
-    #                          cl.CL_MEM_WRITE_ONLY,
-    #                          ctypes.sizeof(z),
-    #                          None,
-    #                          ctypes.byref(status))
-    # cl_utils.cl_check(status.value)
-
-    # lib.parallel_add(bufx, bufy, bufz, len(py_z))
-    # cl.clFinish(cl_cmd_queue)
-
-    # cl.clEnqueueReadBuffer(cl_cmd_queue,
-    #                        bufz,
-    #                        cl.CL_TRUE,
-    #                        0,
-    #                        ctypes.sizeof(z),
-    #                        ctypes.byref(z),
-    #                        0,
-    #                        None,
-    #                        None)
-
-    # assert z[0] == 9 and z[1] == 14 and z[2] == 18
 
 def test_atomic_add():
     with open('loma_code/atomic_add.py') as f:
