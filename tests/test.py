9e77f509b18b24585577a659b2b9c362435b0d80
Tzu-Mao Li <tzumao@mit.edu>
2024-04-23 21:37:53 -0700

    test z type

--- tests/test.py
+++ tests/test.py
@@ -328,9 +328,10 @@ def test_parallel_add():
     x = (ctypes.c_int * len(py_x))(*py_x)
     py_y = [7, 11, 13]
     y = (ctypes.c_int * len(py_y))(*py_y)
-    py_z = [0, 0, 0]
-    z = (ctypes.c_int * len(py_z))(*py_z)
-    lib.parallel_add(x, y, z, len(py_z))
+    #py_z = [0, 0, 0]
+    z_type = (ctypes.c_int * 3)
+    z = z_type()
+    lib.parallel_add(x, y, z, 3)
     print(*x)
     print(*y)
     print(*z)
