83ae21f27b59f37785389272451da64c94a6f44b
Tzu-Mao Li <tzumao@mit.edu>
2024-04-23 22:40:43 -0700

    z[i] = 5

--- tests/loma_code/parallel_add.py
+++ tests/loma_code/parallel_add.py
@@ -3,4 +3,4 @@ def parallel_add(x : In[Array[int]],
                  y : In[Array[int]],
                  z : Out[Array[int]]):
     i : int = thread_id()
-    z[i] = x[i] + y[i]
+    z[i] = 5 #x[i] + y[i]
