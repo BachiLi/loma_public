979089530aaeef111e07461542f2dbe3ac99563d
Tzu-Mao Li <tzumao@mit.edu>
2026-03-30 10:50:11 -0700

    Update handout

--- slang_utils.py
+++ slang_utils.py
@@ -2,12 +2,12 @@ import slangpy
 import platform
 
 def create_slang_device():
-	"""
-		A wrapper for generating a slang device using slangpy
-		Use Metal when we're using MacOS and use SPIR-V when we're using Windows/Linux
-	"""
-	if platform.system() == 'Darwin':
-		device = slangpy.Device(type = slangpy.DeviceType.metal)
-	else:
-		device = slangpy.Device(type = slangpy.DeviceType.vulkan)
-	return device
+    """
+        A wrapper for generating a slang device using slangpy
+        Use Metal when we're using MacOS and use SPIR-V when we're using Windows/Linux
+    """
+    if platform.system() == 'Darwin':
+        device = slangpy.Device(type = slangpy.DeviceType.metal)
+    else:
+        device = slangpy.Device(type = slangpy.DeviceType.vulkan)
+    return device
